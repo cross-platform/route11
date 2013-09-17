013d2b49fc8e367b181a541d434c391f50720eb7
Marcus Tomlinson <marcus@marcus-Parallels-Virtual-Platform>
2013-09-17 11:21:34 +0200

    Formatting

--- R11ComponentThread.h
+++ R11ComponentThread.h
@@ -16,98 +16,20 @@ class R11ComponentThread
 {
 public:
   R11ComponentThread() = default;
-  R11ComponentThread( const R11ComponentThread& other ) {}
+  R11ComponentThread( const R11ComponentThread& other );
 
-  //-----------------------------------------------------------------------------
+  ~R11ComponentThread();
 
-  ~R11ComponentThread()
-  {
-    Stop();
-  }
+  void Initialise( std::function< void( char ) > tickMethod );
 
-  //-----------------------------------------------------------------------------
+  void Sync();
 
-  void Initialise( std::function< void( char ) > tickMethod )
-  {
-    _tickMethod = tickMethod;
-  }
+  void Resume();
 
-  //-----------------------------------------------------------------------------
-
-  void Sync()
-  {
-    _resumeMutex.lock();
-
-    if( !_gotSync ) // if haven't already got sync
-    {
-      _syncCondt.wait( _resumeMutex ); // wait for sync
-    }
-
-    _resumeMutex.unlock();
-  }
-
-  //-----------------------------------------------------------------------------
-
-  void Resume()
-  {
-    _resumeMutex.lock();
-
-    _gotSync = false; // reset the sync flag
-
-    _gotResume = true; // set the resume flag
-    _resumeCondt.notify_all();
-
-    _resumeMutex.unlock();
-  }
-
-  //-----------------------------------------------------------------------------
-
-  void Stop()
-  {
-    _stop = true;
-
-    while( !_stopped )
-    {
-      _syncCondt.notify_all();
-      _resumeCondt.notify_all();
-      std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
-    }
-
-    _thread.join();
-  }
+  void Stop();
 
 private:
-  //-----------------------------------------------------------------------------
-
-  void _ThreadTick()
-  {
-    while( !_stop )
-    {
-      _resumeMutex.lock();
-
-      // notify sync
-      _gotSync = true; // set the sync flag
-      _syncCondt.notify_all();
-
-      // wait for resume
-      if( !_gotResume ) // if haven't already got resume
-      {
-        _resumeCondt.wait( _resumeMutex ); // wait for resume
-      }
-      _gotResume = false; // reset the resume flag
-
-      _resumeMutex.unlock();
-
-      if( !_stop && _tickMethod != nullptr )
-      {
-        _tickMethod( 0 );
-      }
-    }
-
-    _stopped = true;
-  }
-
-  //-----------------------------------------------------------------------------
+  void _ThreadTick();
 
 private:
   std::function< void( char ) > _tickMethod = nullptr;
@@ -121,6 +43,98 @@ private:
   std::condition_variable_any _syncCondt;
   std::thread _thread = std::thread( &R11ComponentThread::_ThreadTick, this );
 };
+
+//=============================================================================
+
+R11ComponentThread::R11ComponentThread( const R11ComponentThread& other ) {}
+
+//-----------------------------------------------------------------------------
+
+R11ComponentThread::~R11ComponentThread()
+{
+  Stop();
+}
+
+//=============================================================================
+
+void R11ComponentThread::Initialise( std::function< void( char ) > tickMethod )
+{
+  _tickMethod = tickMethod;
+}
+
+//-----------------------------------------------------------------------------
+
+void R11ComponentThread::Sync()
+{
+  _resumeMutex.lock();
+
+  if( !_gotSync ) // if haven't already got sync
+  {
+    _syncCondt.wait( _resumeMutex ); // wait for sync
+  }
+
+  _resumeMutex.unlock();
+}
+
+//-----------------------------------------------------------------------------
+
+void R11ComponentThread::Resume()
+{
+  _resumeMutex.lock();
+
+  _gotSync = false; // reset the sync flag
+
+  _gotResume = true; // set the resume flag
+  _resumeCondt.notify_all();
+
+  _resumeMutex.unlock();
+}
+
+//-----------------------------------------------------------------------------
+
+void R11ComponentThread::Stop()
+{
+  _stop = true;
+
+  while( !_stopped )
+  {
+    _syncCondt.notify_all();
+    _resumeCondt.notify_all();
+    std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+  }
+
+  _thread.join();
+}
+
+//=============================================================================
+
+void R11ComponentThread::_ThreadTick()
+{
+  while( !_stop )
+  {
+    _resumeMutex.lock();
+
+    // notify sync
+    _gotSync = true; // set the sync flag
+    _syncCondt.notify_all();
+
+    // wait for resume
+    if( !_gotResume ) // if haven't already got resume
+    {
+      _resumeCondt.wait( _resumeMutex ); // wait for resume
+    }
+    _gotResume = false; // reset the resume flag
+
+    _resumeMutex.unlock();
+
+    if( !_stop && _tickMethod != nullptr )
+    {
+      _tickMethod( 0 );
+    }
+  }
+
+  _stopped = true;
+}
 
 }
 
